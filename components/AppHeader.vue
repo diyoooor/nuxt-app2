44688ae33f7e6b656c5bbd0de43e522a76361747
chutiphong.cha <chutiphong.charoenchao@gmail.com>
2025-06-14 13:53:45 +0700

    feat: init layout

---
+++ components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-primary w-full">
+    <nav class="container mx-auto">
+      <h1>App Header</h1>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-primary min-h-screen">
+    <AppHeader />
+    <main class="container mx-auto bg-primary">
+      <slot />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>

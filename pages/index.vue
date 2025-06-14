5eba6b0f7f5a402896aa7649412e4d7365b48942
chutiphong.cha <chutiphong.charoenchao@gmail.com>
2025-06-14 15:53:53 +0700

    feat: add svg thailand

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Hello World</h1>
+    <section class="grid grid-cols-3 gap-4">
+      <AppCard v-for="card in cards" :key="card.title" :title="card.title" :description="card.description" :link="card.link" />
+    </section>
   </div>
 </template>
 
@@ -8,6 +11,23 @@
 definePageMeta({
   layout: 'default'
 })
+const cards = [
+  {
+    title: 'Card 1',
+    description: 'This is the first card',
+    link: '/travel'
+  },
+  {
+    title: 'Card 2',
+    description: 'This is the second card',
+    link: '/card2'
+  },
+  {
+    title: 'Card 3',
+    description: 'This is the third card',
+    link: '/card3'
+  }
+]
 </script>
 
 <style>
